20cb12b1be5b7298cb4c85c50c6e390a35d47be3
Jaolmos <jose.a.olmosmartinez@gmail.com>
2023-08-22 22:45:20 +0200

    test negative is added for password

--- tests/test_login_negative.py
+++ tests/test_login_negative.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 class TestNegativeScenarios:
+
     @pytest.mark.login
     @pytest.mark.negative
     def test_negative_username(self):
@@ -14,7 +15,7 @@ class TestNegativeScenarios:
 
         # Go to webpage "https://practicetestautomation.com/practice-test-login/"
         driver.get("https://practicetestautomation.com/practice-test-login/")
-        time.sleep(2)
+        time.sleep(4)
 
         # Type username incorrectUser into Username field
         username_locator = driver.find_element(By.ID, "username")
@@ -24,7 +25,7 @@ class TestNegativeScenarios:
         password_locator = driver.find_element(By.NAME, "password")
         password_locator.send_keys("Password123")
 
-        # Puch Submit button
+        # PushSubmit button
         submit_button_locator = driver.find_element(By.XPATH, "/html//button[@id='submit']")
         submit_button_locator.click()
         time.sleep(2)
@@ -36,3 +37,35 @@ class TestNegativeScenarios:
         # Verify error message text is Your username is invalid!
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not expected"
+
+    @pytest.mark.login
+    @pytest.mark.negative
+    def test_negative_password(self):
+        # Open page
+        driver = webdriver.Chrome()
+        time.sleep(2)
+
+        # Go to webpage "https://practicetestautomation.com/practice-test-login/"
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+        time.sleep(4)
+
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        # Type password incorrectPassword into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("incorrectPassword")
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "/html//button[@id='submit']")
+        submit_button_locator.click()
+        time.sleep(4)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should be "
+
+        # Verify error message text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == "Your password is invalid!", "Error message is not expected"
