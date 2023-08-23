b7b66aa41d470b685892c5722485f055e63824ea
Jaolmos <jose.a.olmosmartinez@gmail.com>
2023-08-23 19:42:20 +0200

    I add functionality to perform tests with two browsers at the same time

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
-@pytest.fixture()
-def driver():
-    print("Creating Chrome Driver")
-    my_driver = webdriver.Chrome()
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    #browser = request.config.getoption("--browser")
+    browser = request.param
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox()
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
     yield my_driver
-    print("Closing Chrome Driver")
+    print("Closing {browser} Driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="firefox", help="browser to execute tests (chrome or firefox"
+    )
