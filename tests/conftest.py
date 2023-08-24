8bc463623dbbe646336a9cde0c514987225cfd2f
Jaolmos <jose.a.olmosmartinez@gmail.com>
2023-08-24 16:31:09 +0200

    Explicit wait added

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def driver(request):
         my_driver = webdriver.Firefox()
     else:
         raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
-    my_driver.implicitly_wait(10)
+    #my_driver.implicitly_wait(10)
     yield my_driver
     print("Closing {browser} Driver")
     my_driver.quit()
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,6 +2,8 @@ import time
 
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as excond
 
 
 class TestExceptions:
@@ -10,13 +12,15 @@ class TestExceptions:
     def test_no_such_element_exception(self, driver):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
-        #time.sleep(6)
+
 
         # Click Add button
         add_button_locator = driver.find_element(By.ID, "add_btn")
         add_button_locator.click()
-        #time.sleep(10)
+
+        wait = WebDriverWait(driver, 10)
+        row_2_input_element = wait.until(excond.presence_of_element_located((By.XPATH, "//div[@id='row2']/input")))
 
         # Verify Row 2 input field is displayed
-        row_2_input_locator = driver.find_element(By.XPATH, "//div[@id='row2']/input")
-        assert row_2_input_locator.is_displayed(), "Row 2 input should be displayed, but it's not"
+        #row_2_input_locator= driver.find_element(By.XPATH, "//div[@id='row2']/input")
+        assert row_2_input_element.is_displayed(), "Row 2 input should be displayed, but it's not"
